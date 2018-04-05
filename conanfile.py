03c65322f82c91fb60e7cbcc549001de97f17854
danimtb <danimanzaneque@gmail.com>
2018-04-05 13:16:29 +0200

    sys req for macos, gitignore adn license

--- conanfile.py
+++ conanfile.py
@@ -6,14 +6,19 @@ from conans.tools import os_info, SystemPackageTool
 class LapackConan(ConanFile):
     name = "lapack"
     version = "3.7.1"
+    license = "BSD 3-Clause"
+    homepage = "https://github.com/Reference-LAPACK/lapack"
     description = """LAPACK is a library of Fortran subroutines for solving the most commonly
 occurring problems in numerical linear algebra"""
     url = "https://github.com/conan-community/conan-lapack"
-    homepage = "https://github.com/Reference-LAPACK/lapack"
     settings = "os", "arch", "compiler", "build_type"
     options = {"CMAKE_GNUtoMS": [True, False]}
     default_options = "CMAKE_GNUtoMS=False"
     generators = "cmake"
+
+    @property
+    def source_subfolder(self):
+        return "sources"
 
     def package_id(self):
         if self.options.CMAKE_GNUtoMS:
@@ -28,12 +33,11 @@ occurring problems in numerical linear algebra"""
     def source(self):
         source_url = ("%s/archive/v%s.zip" % (self.homepage, self.version))
         tools.get(source_url)
-        os.rename("%s-%s" % (self.name, self.version), "sources")
+        os.rename("%s-%s" % (self.name, self.version), self.source_subfolder)
         tools.replace_in_file("sources/CMakeLists.txt", "project(LAPACK Fortran C)",
                               """project(LAPACK Fortran C)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
-""")
+conan_basic_setup()""")
 
     def build_requirements(self):
         if self.settings.os == "Windows":
@@ -43,7 +47,8 @@ conan_basic_setup()
         if os_info.is_linux:
             installer = SystemPackageTool()
             installer.install("gfortran")
-        if os_info.is_macos:
+        if (os_info.is_macos and self.settings.compiler == "apple-clang" and
+                self.settings.compiler.version in ["8.1", "9.0"]):
             installer = SystemPackageTool()
             installer.install("gcc", update=True, force=True)
 
@@ -54,16 +59,16 @@ conan_basic_setup()
         cmake.definitions["BUILD_TESTING"] = False
         cmake.definitions["LAPACKE"] = True
         cmake.definitions["CBLAS"] = True
-        cmake.configure(source_folder="sources")
+        cmake.configure(source_folder=self.source_subfolder)
         cmake.build(target="blas")
         cmake.build(target="cblas")
         cmake.build(target="lapack")
         cmake.build(target="lapacke")
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src="sources", ignore_case=True,
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder, ignore_case=True,
                   keep_path=False)
-        self.copy(pattern="*.h", dst="include", src="sources", keep_path=False)
+        self.copy(pattern="*.h", dst="include", src=self.source_subfolder, keep_path=False)
         self.copy(pattern="*blas*.dll", dst="bin", src="bin", keep_path=False)
         self.copy(pattern="*lapack*.dll", dst="bin", src="bin", keep_path=False)
         self.copy(pattern="*blas*.so*", dst="lib", src="lib", keep_path=False)
