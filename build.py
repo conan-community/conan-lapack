aefe91250673a1f514013b2107fa887085905f20
Uilian Ries <uilianries@gmail.com>
2019-03-20 14:47:25 -0300

    Fix 2/N Build Visual Studio
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import copy
 from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     if os.getenv("BUILD_VISUAL_STUDIO"):
-        options = {"visual_studio": True, "shared": True}
+        options = {"lapack:visual_studio": True, "lapack:shared": True}
         settings = {"compiler": "gcc", "compiler.version": "7", "os": "Windows", "arch": "x86_64"}
         for build_type in ["Release", "Debug"]:
             settings.update({"build_type": build_type})
-            builder.add(settings, options, {}, {})
+            builder.add(copy.copy(settings), options, {}, {})
     else:
         builder.add_common_builds()
     builder.run()
